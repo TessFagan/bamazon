b20b3203effcfbae0d921fea7cccdcd2b300d5d2
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-06-15 16:34:33 -0400

    added in products to the sql db and started working on the .js file

---
+++ bamazom.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255),
+  department_name VARCHAR (255),
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Chocolate', 'Food', 2.00, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Broom', 'Cleaning Supplies', 10.00, 14000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Milk', 'Food', 2.00, 9000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Jeans', 'Clothes', 40.00, 2000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('The Art of War', 'Books', 20.00, 40000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('iPad', 'Electronics', 300.00, 500);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Settlers of Catan', 'Games', 30.00, 5000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Womens Socks', 'Clothes', 5.00, 20000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Cat Nip', 'Pet', 2.00, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Piano', 'Music', 800.00, 300);
+
+SHOW TABLES;
+SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,26 @@
 const inquirer = require("inquirer")
+const prettycolumns = require("pretty-columns")
+const mysql = require("mysql")
+
+// MySQL DB Connection Information:
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+
+
+
+
